a51f23f617a1027ef4b027b61a65ae7f6d33a879
jonschwa <jon.schwartz@leaflink.com>
2021-12-03 17:48:11 -0500

    day3 part 2

--- year2021/3.go
+++ year2021/3.go
@@ -6,7 +6,8 @@ import (
 	"strconv"
 )
 
-type Container [][2]int
+type BitCriteria [2]int
+type Container []BitCriteria
 
 func RunDay3() {
 	data, err := helpers.GetDataForDay(2021, 3)
@@ -18,7 +19,7 @@ func RunDay3() {
 	fmt.Println("Part 2:", solveDay3Part2(data))
 }
 
-func getRatesFromContainer(c Container) (int64, int64) {
+func getPowerRatesFromContainer(c Container) (int64, int64) {
 	var epsilonRate, gammaRate string
 	for _, counts := range c {
 		if counts[0] > counts[1] {
@@ -42,7 +43,7 @@ func getRatesFromContainer(c Container) (int64, int64) {
 	return epsilonInt, gammaInt
 }
 
-func solveDay3Part1(data []string) int64 {
+func makeContainer(data []string) Container {
 	container := make(Container, len(data[0]))
 	for _, chars := range data {
 		for j, char := range chars {
@@ -53,11 +54,98 @@ func solveDay3Part1(data []string) int64 {
 			container[j][intChar]++
 		}
 	}
+	return container
+}
 
-	epsilonRate, gammaRate := getRatesFromContainer(container)
+func solveDay3Part1(data []string) int64 {
+	container := makeContainer(data)
+	epsilonRate, gammaRate := getPowerRatesFromContainer(container)
 	return epsilonRate * gammaRate
 }
 
+func getBitCriteria(idx int, data []string, ratingType string) int {
+	var bitCriteria int
+	var bitCriteriaContainer BitCriteria
+	for _, chars := range data {
+		intChar, err := strconv.Atoi(string(chars[idx]))
+		if err != nil {
+			panic(err)
+		}
+		if intChar == 0 {
+			bitCriteriaContainer[0]++
+		} else {
+			bitCriteriaContainer[1]++
+		}
+	}
+	if ratingType == "o2" {
+		// o2 rating uses the most common (or 1 as the tie breaker)
+		if bitCriteriaContainer[0] > bitCriteriaContainer[1] {
+			bitCriteria = 0
+		} else if bitCriteriaContainer[0] < bitCriteriaContainer[1] {
+			bitCriteria = 1
+		} else {
+			bitCriteria = 1
+		}
+	} else if ratingType == "co2" {
+		// co2 rating uses the least common (or 0 as the tie breaker)
+		if bitCriteriaContainer[0] > bitCriteriaContainer[1] {
+			bitCriteria = 1
+		} else if bitCriteriaContainer[0] < bitCriteriaContainer[1] {
+			bitCriteria = 0
+		} else {
+			bitCriteria = 0
+		}
+	}
+
+	return bitCriteria
+}
+
+func getUpdatedData(idx int, bitCriteria int, data []string) []string {
+	newData := data[:0]
+	for _, chars := range data {
+		intChar, err := strconv.Atoi(string(chars[idx]))
+		if err != nil {
+			panic(err)
+		}
+		if intChar == bitCriteria {
+			newData = append(newData, chars)
+		}
+	}
+	return newData
+}
+
+func filterBitsByCriteria(idx int, data []string, ratingType string) string {
+	i := idx
+	bitCriteria := getBitCriteria(i, data, ratingType)
+	newData := getUpdatedData(idx, bitCriteria, data)
+
+	if len(newData) > 1 {
+		i++
+		return filterBitsByCriteria(i, newData, ratingType)
+	}
+	return newData[0]
+}
+
 func solveDay3Part2(data []string) int64 {
-	return 0
+	o2Data := make([]string, len(data))
+	co2Data := make([]string, len(data))
+
+	// making copies because data is a value type, and gets mutated in the recursive functions above,
+	// which is v annoying. TODO - there must be a more idiomatic way to do this
+	copy(o2Data, data)
+	copy(co2Data, data)
+
+	o2Rating := filterBitsByCriteria(0, o2Data, "o2")
+	co2Rating := filterBitsByCriteria(0, co2Data, "co2")
+
+	o2Int, err := strconv.ParseInt(o2Rating, 2, 64)
+	if err != nil {
+		panic(err)
+	}
+	co2Int, err := strconv.ParseInt(co2Rating, 2, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return o2Int * co2Int
 }
--- year2021/3_test.go
+++ year2021/3_test.go
@@ -36,5 +36,27 @@ func TestDay3Part1(t *testing.T) {
 }
 
 func TestDay3Part2(t *testing.T) {
+	var testCases = []testCaseDay3{
+		{[]string{
+			"00100",
+			"11110",
+			"10110",
+			"10111",
+			"10101",
+			"01111",
+			"00111",
+			"11100",
+			"10000",
+			"11001",
+			"00010",
+			"01010",
+		}, 230},
+	}
 
+	for _, testCase := range testCases {
+		result := solveDay3Part2(testCase.input)
+		if result != testCase.expected {
+			t.Errorf("Expected %d, but got: %d", testCase.expected, result)
+		}
+	}
 }
